61ba58eeb457c379eeeec18c74a4dd4042261608
edy17 <edy.diehl@Outlook.fr>
2019-12-19 05:26:46 +0100

    2-opt

--- TME_TSP2D_20191213/src/algorithms/DefaultTeam.java
+++ TME_TSP2D_20191213/src/algorithms/DefaultTeam.java
@@ -5,6 +5,7 @@ import supportGUI.Line;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DefaultTeam {
 
@@ -13,56 +14,46 @@ public class DefaultTeam {
         double bestDist = score(points);
         double newDist;
         int swaps = 1;
-        int improve = 0;
-        int iterations = 0;
-        long comparisons = 0;
 
         while (swaps != 0) { //loop until no improvements are made.
             swaps = 0;
-
             //initialise inner/outer loops avoiding adjacent calculations and making use of problem symmetry to half total comparisons.
             for (int i = 1; i < points.size() - 2; i++) {
                 for (int j = i + 1; j < points.size() - 1; j++) {
-                    comparisons++;
                     //check distance of line A,B + line C,D against A,C + B,D if there is improvement, call swap method.
                     if ((points.get(i).distance(points.get(i - 1)) + points.get(j + 1).distance(points.get(j))) >=
                             (points.get(i).distance(points.get(j + 1)) + points.get(i - 1).distance(points.get(j)))) {
-
                         newTour = swap(points, i, j); //pass arraylist and 2 points to be swapped.
-
                         newDist = score(newTour);
-
                         if (newDist < bestDist) { //if the swap results in an improved distance, increment counters and update distance/tour
                             points = newTour;
                             bestDist = newDist;
                             swaps++;
-                            improve++;
                         }
                     }
                 }
             }
-            iterations++;
         }
         return points;
     }
 
-    private static ArrayList<Point> swap(ArrayList<Point> cities, int i, int j) {
+    private static ArrayList<Point> swap(ArrayList<Point> points, int i, int j) {
         //conducts a 2 opt swap by inverting the order of the points between i and j
         ArrayList<Point> newTour = new ArrayList<>();
         //take array up to first point i and add to newTour
-        int size = cities.size();
+        int size = points.size();
         for (int c = 0; c <= i - 1; c++) {
-            newTour.add(cities.get(c));
+            newTour.add(points.get(c));
         }
         //invert order between 2 passed points i and j and add to newTour
         int dec = 0;
         for (int c = i; c <= j; c++) {
-            newTour.add(cities.get(j - dec));
+            newTour.add(points.get(j - dec));
             dec++;
         }
         //append array from point j to end to newTour
         for (int c = j + 1; c < size; c++) {
-            newTour.add(cities.get(c));
+            newTour.add(points.get(c));
         }
         return newTour;
     }
@@ -84,6 +75,27 @@ public class DefaultTeam {
         return p;
     }
 
+    public ArrayList<Point> improveSwap(ArrayList<Point> points) {
+        ArrayList<Point> result = new ArrayList<>();
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 2; j < points.size(); j++) {
+                int n = points.size();
+                double a = points.get(i % n).distance(points.get((i + 1) % n));
+                double b = points.get(j % n).distance(points.get((j + 1) % n));
+                double c = points.get(i % n).distance(points.get((j) % n));
+                double d = points.get((i + 1) % n).distance(points.get((j + 1) % n));
+                if ((a + b) > (c + d)) {
+                    for (int k = 0; k <= i; k++) result.add(points.get(k));
+                    for (int k = j; k > i; k--) result.add(points.get(k));
+                    for (int k = j + 1; k < n; k++) result.add(points.get(k));
+                    return result;
+                }
+            }
+            return points;
+        }
+        return null;
+    }
+
     // calculTSP: ArrayList<Point> --> ArrayList<Point>
     //   renvoie une permutation P de points telle que la visite
     //   de ces points selon l'ordre défini par P est de distance
@@ -92,18 +104,23 @@ public class DefaultTeam {
         if (points.size() < 4) {
             return points;
         }
-        ArrayList<Point> result = new ArrayList<>();
-        ArrayList<Point> rest = (ArrayList<Point>) points.clone();
-        Point dummy = rest.get(0);
-        result.add(dummy);
-        rest.remove(dummy);
-        while (!rest.isEmpty()) {
-            Point p = plusProche(dummy, rest);
-            result.add(p);
-            rest.remove(p);
-            dummy = p;
+        ArrayList<Point> globalBestResult = null;
+        for (int i = 0; i < points.size(); i++) {
+            ArrayList<Point> result = new ArrayList<>();
+            ArrayList<Point> rest = (ArrayList<Point>) points.clone();
+            Point dummy = rest.get(i);
+            result.add(dummy);
+            rest.remove(dummy);
+            while (!rest.isEmpty()) {
+                Point p = plusProche(dummy, rest);
+                result.add(p);
+                rest.remove(p);
+                dummy = p;
+            }
+            result = alternate(result);
+            if((globalBestResult==null)||(score(result) < score(globalBestResult))) globalBestResult = result;
         }
-        return alternate(result);
+        return globalBestResult;
     }
 
 
@@ -116,10 +133,16 @@ public class DefaultTeam {
             return points;
         }
         ArrayList<Point> result = calculTSP(points);
-        Genome globalBestGenome = new Genome(result);
-        Genetic geneticAlgorithm = new Genetic(SelectionType.TOURNAMENT, 0, points);
-        Genome resultGenome = geneticAlgorithm.optimize(globalBestGenome);
-        return resultGenome.points;
+        Collections.shuffle(points);
+        ArrayList<Point> newResult = calculTSP(points);
+        for (int i = 0; i < 10; i++) {
+            if (score(newResult) < score(result)) {
+                result = newResult;
+            }
+            Collections.shuffle(points);
+            newResult = calculTSP(points);
+        }
+        return result;
     }
 
     // calculDiametre: ArrayList<Point> --> Line
